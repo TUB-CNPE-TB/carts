748c05a16ee25d1edd71a8edb45914ee32a3f362
ciphersmaug <39407479+ciphersmaug@users.noreply.github.com>
2022-02-06 18:43:40 +0100

    added artifact download to carts

--- src/main/java/works/weave/socks/cart/CartApplication.java
+++ src/main/java/works/weave/socks/cart/CartApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnablePrometheusEndpoint
+@EnableDataUsageTracing
 public class CartApplication {
     public static void main(String[] args) {
         SpringApplication.run(CartApplication.class, args);
